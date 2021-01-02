3eaa37c605bdba01185660ca89539eb55e2cfad6
Ira Martynova <mirtle@tut.by>
2021-01-02 13:47:50 +0300

    create store

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,18 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
-
-    let {postsPage: p} = props.state;
+    
+    let {postsPage: p, profilePage} = props.state;
     let posts = p.posts.map(post => <Post key={post.id} message={post.message} likesCount={post.likesCount} />);
+    let newPostElement = React.createRef();
 
-    let refText = React.createRef();
+    let addPost = () => {
+        props.setPost()
+    }
 
-    let textMessage = () => {
-        let textValue= refText.current.value;
-        props.setPost(textValue)
+    let changePost = () => {
+        let text= newPostElement.current.value;
+        props.updatePostMessage(text);
     }
 
     return (
@@ -19,10 +22,10 @@ const MyPosts = (props) => {
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea ref={refText} ></textarea>
+                    <textarea ref={newPostElement} onChange={changePost} value={profilePage.newPostText}></textarea>
                 </div>
                 <div>
-                    <button onClick={textMessage}>Add post</button>
+                    <button onClick={addPost} >Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,37 +1,56 @@
-let state = {
-    profilePage:{
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrew'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'}
-    ]},
-    messagesPage: {
-    messages: [
-        {id: 1, message: 'Hi'},
-        {id: 2, message: 'How is your it-kamasutra?'},
-        {id: 3, message: 'Yo'},
-        {id: 4, message: 'Yo'},
-        {id: 5, message: 'Yo'}
-    ]},
-    postsPage:{
-        posts:[
-        { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-        { id: 2, message: 'It\'s my first post', likesCount: 11 }
-    ]}
-}
+let store = {
+    _state: {
+        profilePage:{
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrew'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}
+            ],
+            newPostText: '',
+    },
+        messagesPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'How is your it-kamasutra?'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'}
+        ]},
+        postsPage:{
+            posts:[
+            { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+            { id: 2, message: 'It\'s my first post', likesCount: 11 }
+        ]}
+    },
+    _callSubscribe() {
+        console.log('change')
+    },
+    getState(){
+        return this._state;
+    },
+    setPost() {
+        let post = {
+            id: 3,
+            message: this._state.profilePage.newPostText,
+            likesCount:3
+        }
+        this._state.postsPage.posts.push(post);
+        this._state.profilePage.newPostText = '';
+        this._callSubscribe(this._state);
+    },
+    updatePostMessage(post) {
+        this._state.profilePage.newPostText = post
+        this._callSubscribe(this._state);
+    },
 
-export let setPost = (message) => {
-    let post = {
-        id: 3,
-        message,
-        likesCount:3
+    subscribe(observer) {
+        this._callSubscribe = observer;
     }
-    state.postsPage.posts.push(post);
-    debugger;
 }
 
 
-export default state;
+export default store;
+window.store = store;
